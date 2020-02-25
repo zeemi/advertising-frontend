f8b833c7239e0ad66419676486838cba4e7d39ea
Marcin Siedlarz <marcin.siedlarz@joymile.com>
2020-02-25 10:38:12 +0100

    feat: improve apiClient

--- src/helpers/apiClient.js
+++ src/helpers/apiClient.js
@@ -2,7 +2,12 @@ import settings from "../settings";
 
 export default class ApiClient {
   static get(path){
-    return fetch(`${settings.backendUrl}${path}`).then(res=> res.json());
+    return fetch(`${settings.backendUrl}${path}`).then(response=> {
+      if (response.status < 400) {
+        return response.json()
+      }
+      return Promise.reject();
+    });
   }
 
   static getMetrics() {
